b4c9abcbca4464f417fa04ccbef0d6b742d1eece
Adam Siwiec <73085927+adamsiwiec1@users.noreply.github.com>
2024-01-10 13:04:03 -0500

    Delete venv directory

--- venv/Lib/site-packages/plivo/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import exceptions
-from .rest import Client as RestClient
-from .rest import phlo
-from . import xml as plivoxml
--- venv/Lib/site-packages/plivo/xml/xmlUtils.py
+++
@@ -1,7 +0,0 @@
-import six
-
-
-def map_type(val):
-    if isinstance(val, bool):
-        return six.text_type(val).lower()
-    return six.text_type(val)
